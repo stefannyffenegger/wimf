50bc6392d867b620f64daf9f52d19a0c228db32f
stefannyffenegger <Stefa@NTD01-WIN10>
2019-11-24 15:33:30 +0100

    Hibernate Config Test

--- src/main/user/UserDao.java
+++ src/main/user/UserDao.java
@@ -14,33 +14,32 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package main.user; 
-import org.hibernate.Session;  
-import org.hibernate.SessionFactory;  
-import org.hibernate.Transaction;  
-import org.hibernate.boot.Metadata;  
-import org.hibernate.boot.MetadataSources;  
-import org.hibernate.boot.registry.StandardServiceRegistry;  
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;  
-   
-public class UserDao {    
-    
-public static int register(User u){    
- int i=0;    
-  
- StandardServiceRegistry ssr = new StandardServiceRegistryBuilder().configure("hibernate.cfg.xml").build();  
- Metadata meta = new MetadataSources(ssr).getMetadataBuilder().build();  
-  
-SessionFactory factory = meta.getSessionFactoryBuilder().build();  
-Session session = factory.openSession();  
-Transaction t = session.beginTransaction();   
-  
-i=(Integer)session.save(u);    
-  
-t.commit();    
-session.close();    
-    
-return i;    
-   
- }    
-}    
+package main.user;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+public class UserDao {
+    public static int register(User u) {
+        int i = 0;
+
+        StandardServiceRegistry ssr = new StandardServiceRegistryBuilder().configure("../config/hibernate.cfg.xml").build();
+        Metadata meta = new MetadataSources(ssr).getMetadataBuilder().build();
+
+        SessionFactory factory = meta.getSessionFactoryBuilder().build();
+        Session session = factory.openSession();
+        Transaction t = session.beginTransaction();
+
+        i = (Integer) session.save(u);
+
+        t.commit();
+        session.close();
+
+        return i;
+    }
+}
