789f789bdb119ce771f0c659cbfb9ed6b3f6515e
stefannyffenegger <Stefa@NTD01-WIN10>
2019-11-29 16:11:19 +0100

    Hibernate Mappings

--- src/main/java/ch/wimf/api/product/Item.java
+++ src/main/java/ch/wimf/api/product/Item.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2019 Stefa
+ * Copyright (C) 2019 Stefan Nyffenegger
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
--- src/main/java/ch/wimf/api/user/UserDao.java
+++ src/main/java/ch/wimf/api/user/UserDao.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2019 stefa
+ * Copyright (C) 2019 Stefan Nyffenegger
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -32,7 +32,7 @@ public class UserDao {
     public static int register(User u) {
         int i = 0;
 
-        StandardServiceRegistry ssr = new StandardServiceRegistryBuilder().configure("../config/hibernate.cfg.xml").build();
+        StandardServiceRegistry ssr = new StandardServiceRegistryBuilder().configure(".main/config/hibernate.cfg.xml").build();
         Metadata meta = new MetadataSources(ssr).getMetadataBuilder().build();
 
         SessionFactory factory = meta.getSessionFactoryBuilder().build();
