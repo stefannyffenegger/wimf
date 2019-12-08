568ccd098e0a1dd543c8c1fa74d3f1600fa8b1c9
stefannyffenegger <Stefa@NTD01-WIN10>
2019-12-08 18:24:45 +0100

    login stuff and more

--- src/main/java/ch/wimf/usermanagement/model/User.java
+++ src/main/java/ch/wimf/usermanagement/model/User.java
@@ -8,70 +8,87 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 /**
- * User.java
- * This is a model class represents a User entity
+ * User.java This is a model class represents a User entity
+ *
  * @author Ramesh Fadatare
  *
  */
-
 @Entity
-@Table(name="users")
+@Table(name = "users")
 public class User {
-	
-	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
-	@Column(name="id")
-	protected int id;
-	
-	@Column(name="username")
-	protected String username;
-	
-	@Column(name="email")
-	protected String email;
-	
-	@Column(name="phone")
-	protected String phone;
-	
-	public User() {
-	}
-	
-	public User(String username, String email, String phone) {
-		super();
-		this.username = username;
-		this.email = email;
-		this.phone = phone;
-	}
 
-	public User(int id, String username, String email, String phone) {
-		super();
-		this.id = id;
-		this.username = username;
-		this.email = email;
-		this.phone = phone;
-	}
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    protected int id;
 
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public String getUsername() {
-		return username;
-	}
-	public void setUsername(String username) {
-		this.username = username;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public String getPhone() {
-		return phone;
-	}
-	public void setPhone(String phone) {
-		this.phone = phone;
-	}
+    @Column(name = "username")
+    protected String username;
+
+    @Column(name = "email")
+    protected String email;
+
+    @Column(name = "phone")
+    protected String phone;
+
+    @Column(name = "password")
+    protected String password;
+
+    public User() {
+    }
+
+    public User(String username, String email, String phone) {
+        super();
+        this.username = username;
+        this.email = email;
+        this.phone = phone;
+    }
+
+    public User(int id, String username, String email, String phone) {
+        super();
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.phone = phone;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
